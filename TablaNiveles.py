a7db1202d224feed494c6edcc94f3680ae1798cd
antoniotf <antoniodelgado@antoniodelgado.info>
2023-01-20 15:25:36 +0000

    Actualizacion 20/01/2023 15:25

---
+++ TablaNiveles.py
@@ -0,0 +1,326 @@
+
+
+#medida nivelse obligatorio 16 columnas x 8 filas.
+niveles =[]
+
+level_0 = [
+"XXXXX X   X XXXX"
+"  X   X   X X   "
+"  X   XXXXX XXX "
+"  X   X   X X   "
+"  X   X   X XXXX"
+"                "
+"XXXX X   X XXX  "
+"X    XX  X X  X "
+"XXX  X X X X  X "
+"X    X  XX X  X "
+"XXXX X   X XXX  "
+]
+
+level_1 = [
+"      XXX       "
+"   XXXXEXXXX    "
+" XXX @ @   XXX  "
+"XX    @@@ @  XX "
+"X@ @ @ @   @ @X "
+"X@  @ @@@@@  @X "
+"X@@@ @ P@  @@@X "
+"XXXXXXXXXXXXXXX "
+"                "
+"                "
+"                "
+]
+
+level_2 = [
+"XXXXXXXXXXXXXXX "
+"X  @ @   @ @ EX "
+"X  @ @ @@@@ @ X "
+"XP @ @   @ @  X "
+"X  @ @ @@ @ @ X "
+"X  @ @   @ @ @X "
+"X  @ @ @@ @ @ X "
+"XXXXXXXXXXXXXXX "
+"                "
+"                "
+"                "
+]
+
+level_3 = [
+" XXXXXXXXXXXXXX "
+" X   @@     @ X "
+" X      @@ @ @X "
+" X   @@ @ @ @ X "
+" X  @@  @@@@@ X "
+" XP @ @ @     X "
+" X   @ @  @  @X "
+" X    @ @ @@@@X "
+" X   @@  @@@E X "
+" X   @@  @    X "
+" XXXXXXXXXXXXXX "
+]
+
+level_4 = [
+"         XXXX   "
+"      X X    X  "
+"   XXX XX@@ @ X "
+"  X XX X  @   X "
+"  X   @@   @ @X "
+"  X@ @ @   @@ X "
+"  X XX X @@ @@X "
+"   XXXPXX @  EX "
+"      X X@   X  "
+"       X XXXX   "
+"                "
+]
+
+level_5 = [
+"        X       "
+"       XEX      "
+"      X   X     "
+"     X @@@ X    "
+"    X       X   "
+"   X @@@@@@@ X  "
+"    X  @ @  X   "
+"     X  @  X    "
+"      X   X     "
+"       XPX      "
+"        X       "
+]
+
+level_6 = [
+"  XXXXXXXXXXXX  "
+"  X       @  X  "
+"  X@  @  @   X  "
+"  X @@@@@ @@@X  "
+"  X @  @ @@ PX  "
+"  X @  @ @  @X  "
+"  XE @   X@  X  "
+"  XXXXXXXXXXXX  "
+"                "
+"                "
+"                "
+]
+
+level_7 = [
+"   X X X X X X  "
+"  X X X X X X X "
+" X P@ @ @ @ @@ X"
+"  X  @         X"
+" X @@@@@@@@@@@XX"
+"X   @  @       X"
+" X X  @ @ @ @@XX"
+"  X @  @@ @ @  X"
+" X  @ @ @ @ @ @X"
+"  X EX X X X XX "
+"   X  X X X X   "
+]
+
+level_8 = [
+" XXXXXXXXXX     "
+" X        X     "
+" X XXXXXX X XX  "
+" X X  @ X  X X  "
+" X X  @  @@@  X "
+" X P@  @@   @@X "
+" X X  @  @ @ EX "
+" X X  @@X @X XX "
+" X XXXXXX XX    "
+" X        XX@   "
+" XXXXXXXXXX     "
+]
+
+level_9 = [
+"  XXXXXXXXXXXX  "
+"  XXE@ @ @  XX  "
+"  X  @ @ @ @ X  "
+"  X X  @     X  "
+"  X @    @ @ X  "
+"  X@ @@ @ @@@X  "
+"  X @ @@@@ @ X  "
+"  X   @   @  X  "
+"  X @ @ @ @  X  "
+"  XXP   @ @ XX  "
+"  XXXXXXXXXXXX  "
+]
+
+level_10= [
+"       XXX      "
+"     XXX XXX    "
+"   XXX @   XXX  "
+" XXX @  @ @  XXX"
+"XX  @ @  @  @  X"
+"XP @ @ @@@@@@  E"
+"XX  @ @@ @  @ @X"
+" XXX @  @  @ XXX"
+"   XXX @   XXX  "
+"     XXX XXX    "
+"       XXX      "
+]
+
+level_11 = [
+"      XXX       "
+"   XXXXEXXXX    "
+" XXX R R   XXX  "
+"XX    RRR R  XX "
+"XR R R R   R RX "
+"XR  R RRRRR  RX "
+"XRRR R PR  RRRX "
+"XXXXXXXXXXXXXXX "
+"                "
+"                "
+"                "
+]
+
+level_12 = [
+"XXXXXXXXXXXXXXX "
+"X  R R   R R EX "
+"X  R R RRRR R X "
+"XP R R   R R  X "
+"X  R R RR R R X "
+"X  R R   R R RX "
+"X  R R RR R R X "
+"XXXXXXXXXXXXXXX "
+"                "
+"                "
+"                "
+]
+
+level_13 = [
+" XXXXXXXXXXXXXX "
+" X   RR     R X "
+" X      RR R RX "
+" X   RR R R R X "
+" X  RR  RRRRR X "
+" XP R R R     X "
+" X   R R  R  RX "
+" X    R R RRRRX "
+" X   RR  RRRE X "
+" X   RR  R    X "
+" XXXXXXXXXXXXXX "
+]
+
+level_14 = [
+"         XXXX   "
+"      X X    X  "
+"   XXX XXRR R X "
+"  X XX X  R   X "
+"  X   RR   R RX "
+"  XR R R   RR X "
+"  X XX X RR RRX "
+"   XXXPXX R  EX "
+"      X XR   X  "
+"       X XXXX   "
+"                "
+]
+
+level_15 = [
+"        X       "
+"       XEX      "
+"      X   X     "
+"     X RRR X    "
+"    X       X   "
+"   X RRRRRRR X  "
+"    X  R R  X   "
+"     X  R  X    "
+"      X   X     "
+"       XPX      "
+"        X       "
+]
+
+level_16 = [
+"  XXXXXXXXXXXX  "
+"  X       R  X  "
+"  XR  R  R   X  "
+"  X RRRRR RRRX  "
+"  X R  R RR PX  "
+"  X R  R R  RX  "
+"  XE R   XR  X  "
+"  XXXXXXXXXXXX  "
+"                "
+"                "
+"                "
+]
+
+level_17 = [
+"   X X X X X X  "
+"  X X X X X X X "
+" X PR R R R RR X"
+"  X  R         X"
+" X RRRRRRRRRRRXX"
+"X   R  R       X"
+" X X  R R R RRXX"
+"  X R  RR R R  X"
+" X  R R R R R RX"
+"  X EX X X X XX "
+"   X  X X X X   "
+]
+
+level_18 = [
+" XXXXXXXXXX     "
+" X        X     "
+" X XXXXXX X XX  "
+" X X  R X  X X  "
+" X X  R  RRR  X "
+" X PR  RR   RRX "
+" X X  R  R R EX "
+" X X  RRX RX XX "
+" X XXXXXX XX    "
+" X        XXR   "
+" XXXXXXXXXX     "
+]
+
+level_19 = [
+"  XXXXXXXXXXXX  "
+"  XXER R R  XX  "
+"  X  R R R R X  "
+"  X X  R     X  "
+"  X R    R R X  "
+"  XR RR R RRRX  "
+"  X R RRRR R X  "
+"  X   R   R  X  "
+"  X R R R R  X  "
+"  XXP   R R XX  "
+"  XXXXXXXXXXXX  "
+]
+
+level_20= [
+"       XXX      "
+"     XXX XXX    "
+"   XXX R   XXX  "
+" XXX R  R R  XXX"
+"XX  R R  R  R  X"
+"XP R R RRRRRR  E"
+"XX  R RR R  R RX"
+" XXX R  R  R XXX"
+"   XXX R   XXX  "
+"     XXX XXX    "
+"       XXX      "
+]
+
+niveles.append(level_0)
+niveles.append(level_1)
+niveles.append(level_2)
+niveles.append(level_3)
+niveles.append(level_4)
+niveles.append(level_5)
+niveles.append(level_6)
+niveles.append(level_7)
+niveles.append(level_8)
+niveles.append(level_9)
+niveles.append(level_10)
+niveles.append(level_11)
+niveles.append(level_12)
+niveles.append(level_13)
+niveles.append(level_14)
+niveles.append(level_15)
+niveles.append(level_16)
+niveles.append(level_17)
+niveles.append(level_18)
+niveles.append(level_19)
+niveles.append(level_20)
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import itertools
+from TablaNiveles import *
 listado=[]
 repeticiones = 10
 for v in itertools.product([1,2,3,4], repeat=repeticiones):
@@ -7,3 +8,11 @@ for v in itertools.product([1,2,3,4], repeat=repeticiones):
 totalCombinaciones = len (listado)
 
 
+#Cargar laberinto en lista  (x, y, tipo)
+gridJuego= [ [ 0 for i in range(16) ] for j in range(11) ]
+
+gridJuego[1][3]=1
+
+
+
+print (gridJuego)
